710b3a525198ee22b519e338b9aeaa5d1e475ca5
sammad-idenbrid <96107106+sammad-idenbrid@users.noreply.github.com>
2022-11-29 17:32:54 +0500

    empty

--- api.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-Route::post('/login', [App\Http\Controllers\AuthenticationController::class, 'adminLogin']);
-/*LOGOUT*/
-Route::get('/logout', [App\Http\Controllers\AuthenticationController::class, 'logout']);
