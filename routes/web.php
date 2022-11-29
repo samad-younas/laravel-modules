71c67372f57e63de97ae428cc0adda5994926507
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-11-29 18:21:27 +0500

    multi-authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\HomeController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+Route::get('/home', [HomeController::class,'index'])->name('home');
+Route::get('admin/home', [HomeController::class,'handleAdmin'])->name('admin.route')->middleware('admin');
