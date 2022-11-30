3e89deb2171924eed247d207a6ffe8610bea4b5b
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-11-30 17:38:35 +0500

    Update Product.php

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Product extends Model
 {
@@ -11,5 +12,15 @@ class Product extends Model
     public function categories()
     {
         return $this->belongsToMany(Category::class)->withTimestamps();
+    }
+    public function likes(){
+        return $this->hasMany(Like::class,'likeable_id');
+     }
+    public function liked()
+    {
+        // if (is_null($userId)) {
+            $userId = Auth::id();
+        // }
+        return $this->likes()->where('user_id',  auth()->id())->exists();
     }
 }
