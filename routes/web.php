c5d409d2eb9613c3875b10ebc6b8c684e784bb6d
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-11-30 10:21:02 +0500

    authentication merged branch revert

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,3 +18,4 @@ Route::get('/', function () {
 });
 
 Auth::routes();
+
