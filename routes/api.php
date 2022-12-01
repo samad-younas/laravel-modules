a23ffcc096339669c817174a797f8773425c5b20
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-12-01 11:05:29 +0500

    updation web.php

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,3 @@ use App\Http\Controllers\NotificationController;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/send-notification', [NotificationController::class, 'sendOfferNotification']);
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/send-notification', [NotificationController::class, 'sendOfferNotification']);
