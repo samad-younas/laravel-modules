60f20db2efd89d520a335ce86eac24e7ffe543fe
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-12-01 15:10:08 +0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -27,9 +27,6 @@ Route::controller(CategoryController::class)->prefix('/category')->group(functio
     Route::get('/list', 'list');
     Route::get('/remove/{id}', 'destroy');
 });
-
-
-
 Route::controller(ProductController::class)->prefix('/product')->group(function () {
     Route::post('/save', 'store');
     Route::post('/update', 'update');
