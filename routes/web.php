b87e491f4032f216b93f553228cb242739d42b70
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-12-01 11:04:41 +0500

    updation web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('like-product/{id}', [LikeController::class,'like'])->name('like');
