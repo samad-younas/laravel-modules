7095281330b02ca1c89f86d26679c5017258e8b4
Umar Idenbrid <115683699+izmah-idenbrid@users.noreply.github.com>
2022-12-02 10:26:01 +0500

    revert mail to its original laravel code

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,13 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-Route::get('/home', [HomeController::class,'index'])->name('home');
-Route::get('admin/home', [HomeController::class,'handleAdmin'])->name('admin.route')->middleware('admin');
-Route::controller(CategoryController::class)->prefix('/category')->group(function () {
-    Route::post('/save', 'store');
-    Route::post('/edit/{id}', 'edit');
-    Route::post('/update', 'update');
-    Route::get('/list', 'list');
-    Route::get('/remove/{id}', 'destroy');
-});
+
+
+
